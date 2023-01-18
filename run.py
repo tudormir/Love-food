8c1c646b4d59378e115e153ad2b40061dbf4795c
tudormir <tudormir@hotmail.com>
2023-01-18 08:40:45 +0000

    Error met the gspread.

--- run.py
+++ run.py
@@ -40,7 +40,7 @@ def get_sales_data():
             print("Data is valid!")
             break
 
-        
+    return sales_data    
  
 def validate_data(values):
     """ 
@@ -58,5 +58,16 @@ def validate_data(values):
         return False 
     return True     
 
-get_sales_data()
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add now eow with the list data provided.
+    """
+    print("Updating sales worksheet ...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales.worksheet.append.row(data)
+    print("Sales worksheet updated successfully.\n")
 
+data = get_sales_data()
+print(data)
+sales_data=[int(num) for num in data]
+update_sales_worksheet(sales_data)
